5a5111ca54357e40469cd26f2c4bbbea5b7592bf
jesus-dev7 <kids.anguita@gmail.com>
2026-02-09 20:51:30 +0100

    Stuff

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ tableau = [list(ligne) for ligne in laby.splitlines()]
 print(tableau)
 print(laby[0:30])
 init_window(800, 450, "Hello")
-def DwarSquare():
+def DwarSquare(taille_carre, taille_titre):
     while not window_should_close():
         trucenx = 100
         truceny = 100
@@ -14,36 +14,34 @@ def DwarSquare():
         bleu = 0
         begin_drawing()
         clear_background(WHITE)
-        draw_text("Hello world", 190, 200, 20, VIOLET)
+        draw_text("Labyrinthe", trucenx - 50 - taille_titre, truceny- 50 - (taille_titre // 2), taille_titre, RED)
         for truc in tableau:
             for machin in range(len(truc)):
                 Letruc = truc[machin]
-                if Letruc == "X":
-                    rouge = 255
-                    vert = 0
-                    bleu = 0
-                if Letruc == " ":
-                    rouge = 0
-                    vert = 255
-                    bleu = 0
-                if Letruc == "E":
-                    rouge = 0
-                    vert = 0
-                    bleu = 255
-                if Letruc == "S":
-                    rouge = 0
-                    vert = 255
-                    bleu = 255
+                match Letruc:
+                    case "X":
+                        rouge = 255
+                        vert = 0
+                        bleu = 0
+                    case " ":
+                        rouge = 0
+                        vert = 255
+                        bleu = 0
+                    case "E":
+                        rouge = 0
+                        vert = 0
+                        bleu = 255
+                    case "S":
+                        rouge = 0
+                        vert = 255
+                        bleu = 255
                 draw_rectangle_rec(
-                    Rectangle(trucenx + machin*50, truceny, 50, 50),
+                    Rectangle(trucenx + machin*taille_carre, truceny, taille_carre, taille_carre),
                     (rouge, vert, bleu, 255)
                 )
-                if rouge < 255:
-                    rouge += 5
-            vert += 20
-            truceny += 50
+            truceny += taille_carre
         end_drawing()
-DwarSquare()
+DwarSquare(10, 50)
 close_window()
 
 # Set-up : pip3 install raylib==5.5.0.3 --break-system-packages

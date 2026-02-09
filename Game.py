0974bdbf2725b0f34348fa5c44718b7ff8c3a08c
Yolked64 <leobodinleo@gmail.com>
2026-02-09 11:55:39 +0100

    Class
    Added a class for the game

---
+++ Game.py
@@ -0,0 +1,16 @@
+from pyray import *
+
+class Game :
+    def __init__(self):
+        Labyrinth : str = ""
+    def LoadLabyrinth(self, Filepath : str):
+        Labyrinth = str(open(Filepath, "r"))
+    def Launch(self):
+        init_window(800, 450, "Hello")
+    def Update(self):
+        while not window_should_close():
+            begin_drawing()
+            clear_background(WHITE)
+            draw_text("Hello world", 190, 200, 20, VIOLET)
+            end_drawing()
+        close_window()
--- test.py
+++ test.py
@@ -1,13 +1,8 @@
 from pyray import *
-dedale = open("dedales.txt", "r")
-laby = str(dedale.read())
-print(laby[0:9])
-init_window(800, 450, "Hello")
-while not window_should_close():
-    begin_drawing()
-    clear_background(WHITE)
-    draw_text("Hello world", 190, 200, 20, VIOLET)
-    end_drawing()
-close_window()
+from Game import *
+
+game = Game()
+game.Launch()
+game.Update()
 
 # Set-up : pip3 install raylib==5.5.0.3 --break-system-packages

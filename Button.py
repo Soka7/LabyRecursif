0be003cc77cc3015fe3d426756c647d1712f4006
Yolked64 <leobodinleo@gmail.com>
2026-02-11 16:45:51 +0100

    Button
    Added a functional button class allowing easy customizable buttons

---
+++ Button.py
@@ -0,0 +1,120 @@
+from pyray import *
+
+class Button:
+    def __init__(self) -> None:
+        """
+        Create a button object. \n
+        All attributes are defaulted to 0 or null.
+        """
+        self.ButtonColor : Color = (0, 0, 0, 0)
+        self.Position : Vector2 = (0, 0)
+        self.Size : Vector2 = (0, 0)
+        self.Text : str = ""
+        self.TextSize : int = 0
+        self.TextColor : Color = (0, 0, 0, 0)
+        self.HoverSize : int = 0
+        self.HoverColor : Color = (0, 0, 0, 0)
+        return None
+
+    def EditPos(self, Position : Vector2, Size : Vector2) -> None:
+        """
+        Edit the button position and size.
+        
+        :param Position: The x and y position of the button.
+        :type Position: Vector2
+        :param Size: The width and lenght of the button.
+        :type Size: Vector2
+        :return: None
+        """
+        self.Position = Position
+        self.Size = Size
+        return None
+    
+    def IsHovered(self) -> bool:
+        """
+        Check if the mouse is hovering the button.
+        
+        :return: True if the mouse is hovering it, False otherwise.
+        :rtype: bool
+        """
+        if check_collision_point_rec(get_mouse_position(), (self.Position.x, self.Position.y, self.Size.x, self.Size.y)):
+            return True
+        return False
+    
+    def IsClicked(self):
+        """
+        Check if the mouse clicked the button.
+        
+        :return: True if the mouse clicked the button, false otherwise.
+        :rtype: bool
+        """
+        if self.IsHovered() and is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT):
+            return True
+        return False
+    
+    def Draw(self) -> None:
+        """
+        Draw a rectangle being the button at the given coordinates with the given size. \n
+        Also Draw its text and apply a hover effect if needed. \n
+        :return: None
+        """
+        draw_rectangle_rec((self.Position.x, self.Position.y, self.Size.x, self.Size.y), self.ButtonColor)
+        self.DrawText()
+        self.HoverEffect()
+        return None
+
+    def EditText(self, Text : str, TextSize : int, TextColor : Color) -> None:
+        """
+        Edit the text, size of the text and color of the text displayed by the button.
+        
+        :param Text: The text to display.
+        :type Text: str
+        :param TextSize: The fontsize of the text. 
+        :type TextSize: int
+        :param TextColor: The color of the text. (RGBA)
+        :type TextColor: Color
+        :return: None
+        """
+        self.Text = Text
+        self.TextSize = TextSize
+        self.TextColor = TextColor
+        return None
+
+    def EditHover(self, HoverSize : int, HoverColor : Color) -> None:
+        """
+        Edit the hover effect of the button. \n
+        The effect is simply a rectangular border.
+        
+        :param HoverSize: The size of the border in pixels.
+        :type HoverSize: int
+        :param HoverColor: The color of the border (RGBA).
+        :type HoverColor: Color
+        :return: None
+        """
+        self.HoverSize = HoverSize
+        self.HoverColor = HoverColor
+        return None
+    
+    def DrawText(self) -> None:
+        """
+        Draw the text of the button at its center.
+        
+        :return: None
+        """
+        TextWidth : int = measure_text(self.Text, self.TextSize)
+        # Find where the text should be to be centered.
+        TextPosition : Vector2 = Vector2(self.Position.x + (self.Size.x - TextWidth) / 2,
+                                         self.Position.y + (self.Size.y - TextWidth) / 2)
+        draw_text(self.Text, int(TextPosition.x), int(TextPosition.y), self.TextSize, self.TextColor)
+        return None
+
+    def HoverEffect(self) -> None:
+        """
+        Apply the hover effect to the button if it is hovered.
+        
+        :return: None
+        """
+        if self.IsHovered():
+            Object : Rectangle = (self.Position.x, self.Position.y, self.Size.x, self.Size.y)
+            draw_rectangle_lines_ex(Object, self.HoverSize, self.HoverColor)
+        return None
--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 from pyray import *
 from Game import *
+from Button import *
 
 Background : Color = (255, 255, 255, 255)
 ScreenWidth : int = 800
@@ -10,13 +11,21 @@ game : Game = Game()
 
 init_window(ScreenWidth, ScreenHeight, Title)
 
+bouton = Button()
+bouton.EditPos(Vector2(0, 0), Vector2(100, 100))
+bouton.EditText("Moi", 28, (255, 255, 255, 255))
+bouton.EditHover(10, (255, 0, 0, 255))
+
 game.LoadMaze("dedales.txt")
 game.Maze.FindEntry()
 
 while not window_should_close():
     begin_drawing()
     clear_background(Background)
+    if bouton.IsClicked():
+        print("moi")
     game.Draw()
+    bouton.Draw()
     end_drawing()
 
 close_window()

2d128a9358bca006a970fc896944b258996dce9b
Soka7 <kids.anguita@gmail.com>
2026-03-04 11:33:04 +0100

    Comm

--- SolverBotFile.py
+++
@@ -1,9 +0,0 @@
-class SolverBot:
-    def __init__(self, x, y):
-        import os
-        import sys
-        import subprocess
-        self.x = x
-        self.y = y
-        if os.name == "nt":
-            subprocess.Popen(f'start cmd /k python -c "print('X: {self.x}, Y: {self.y}')"', shell=True)
